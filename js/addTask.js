a74ec8924492910a6a5f430a4df8cbf62774019f
Andrey Mirzoev <MirzoAG@gmail.com>
2024-09-06 09:22:24 +0300

    Добавил обработку ошибок

--- js/addTask.js
+++ js/addTask.js
@@ -1,8 +1,23 @@
 import { list } from './../index.js'
 import { STATUS } from './constants.js'
 
-export function addTask(taskText, priority) {
+function stringConverter(taskText) {
 	if (list.some(task => task.name === taskText)) {
+		throw new Error('Задача с таким названием уже существует')
+	}
+
+	if (taskText.length < 3 || taskText.length > 30) {
+		throw new Error('Задача менее 3 или более 30 символов')
+	}
+
+	return taskText
+}
+
+export function addTask(taskText, priority) {
+	try {
+		stringConverter(taskText)
+	} catch (error) {
+		alert(error.message)
 		return
 	}
 
